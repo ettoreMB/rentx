63845323c3944557e9e4f6020d6363f664bf8afd
Ettore <ettore.barbosa@gmail.com>
2021-09-29 18:32:49 -0300

    s3 change

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -6,7 +6,7 @@ import { IStorageProvider } from "./IStorageProvider";
 
 const diskStorage = {
   local: LocalStorageProvider,
-  S3: S3StorageProvider
+  s3: S3StorageProvider
 }
 
 container.registerSingleton<IStorageProvider>(
